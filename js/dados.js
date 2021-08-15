2b211a721271db9a9ccad5415eac0453e5018e51
Miguel Zanona Krasner <miguelzkestudos@gmail.com>
2021-08-15 16:11:28 -0300

    Avançando um pouco no CSS, com uma função criada para os dados

---
+++ js/dados.js
@@ -0,0 +1,6 @@
+function jogarDados(){
+    let dado1 = Math.floor(Math.random() * 6) + 1;
+    let dado2 = Math.floor(Math.random() * 6) + 1;
+    console.log = dado1 + " " + dado2;
+    alert(dado1 + " " + dado2);
+}
